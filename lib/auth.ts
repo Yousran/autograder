1bd2dcac792e01f83f4ab2a9c84e9b3128f8ddf8
Yousran <yusranmazidan@gmail.com>
2026-02-25 16:08:23 +0800

    REF: start fresh to api based

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,5 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
