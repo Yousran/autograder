420ea16ec9f594d5a89739f8607616b26b24e3f4
Yousran <yusranmazidan@gmail.com>
2026-03-29 16:48:57 +0800

    REF: Remove outdated TODOs related to testing and reordering functionality

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,9 @@
-//TODO: uniform test naming, structure and organization in the tests folder
-//TODO: question reordering e2e tests using playwright
 //TODO: ai grading and feedback generation using openrouter as default but with model provider selection support
 //TODO: dont load the choices separately for each question, but load them in bulk with the questions to avoid multiple requests and reordering issues
 //TODO: reordering issues where the question text and type related data goes missing when displaced in the list
 //TODO: nextstepjs tour component for onboarding
 //TODO: scroll to top button on question editing page
 //TODO: scroll to newly created question
-//TODO: e2e tests using playwright
 //TODO: make sure to use next-intl for all text content and add missing translations
 //TODO: add comments to all functions and components
 //TODO: make sure to use centralized types for API routes and form validation
